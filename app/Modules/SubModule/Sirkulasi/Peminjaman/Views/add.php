797631c388176e6e5eb8e042e63387d0f768fea1
ilyasmuhammad25 <dalimarilyas@gmail.com>
2026-01-27 15:16:51 +0700

    fixx entri peminjaman

--- app/Modules/SubModule/Sirkulasi/Peminjaman/Views/add.php
+++ app/Modules/SubModule/Sirkulasi/Peminjaman/Views/add.php
@@ -49,7 +49,19 @@ $request = service('request'); ?>
 				</div>
 			</div>
 			<div class="page-title-actions">
-				<?= view('Member\Views\member_no', array('member_no' => $member_no, 'action' => base_url('sirkulasi-peminjaman/create'))) ?>
+				<form method="get" action="<?= base_url('sirkulasi-peminjaman/create') ?>">
+					<div class="select-wrapper input-group mb-3">
+						<select class="form-control select2" name="member_no" id="member_no" style="min-width:360px">
+							<option value="">Nomor Anggota</option>
+							<?php foreach (get_ref_table('members', 'MemberNo, Fullname', 'MemberNo IS NOT NULL', 'data') as $row) : ?>
+								<option value="<?= $row->MemberNo ?>" <?= $member_no == $row->MemberNo ? 'selected' : '' ?>><?= $row->MemberNo ?> <?= $row->Fullname ?></option>
+							<?php endforeach; ?>
+						</select>
+						<div class="input-group-append">
+							<button class="btn btn-shadow btn bg-corporate-primary2 text-white" type="submit"><i class="fa fa-check-circle"></i> Pilih</button>
+						</div>
+					</div>
+				</form>
 			</div>
 		</div>
 	</div>
