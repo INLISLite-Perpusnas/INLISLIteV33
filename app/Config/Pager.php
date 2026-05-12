d16aec4102b0b175a99bd16e0f1225e7695e40a1
ilyasmuhammad25 <dalimarilyas@gmail.com>
2026-05-12 09:08:27 +0700

    fix case sensitive

--- app/Config/Pager.php
+++ app/Config/Pager.php
@@ -24,7 +24,7 @@ class Pager extends BaseConfig
         'default_full'   => 'CodeIgniter\Pager\Views\default_full',
         'default_simple' => 'CodeIgniter\Pager\Views\default_simple',
         'default_head'   => 'CodeIgniter\Pager\Views\default_head',
-		'opac_pagination' => 'App\Views\Layout\Opac\pager',
+		'opac_pagination' => 'App\Views\layout\opac\pager',
 		'custom_pagination' => 'Layout\Views\frontend\template\arduix5\partial\pager',
     ];
 
